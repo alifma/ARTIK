7450d723ec17d1d7f2ea50c6bf1a736658dbe06c
Alif Maulana Arifin <45408862+alifma@users.noreply.github.com>
2020-03-08 19:26:01 +0700

    Add files via upload

---
+++ SceneLoader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadScene(int level)
+    {
+        Application.LoadLevel(level);
+    }
+}
