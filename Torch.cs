2e20a8b89bb6a1d987dc672c41ba5f07a71dfe11
Alif Maulana Arifin <45408862+alifma@users.noreply.github.com>
2020-03-08 19:25:18 +0700

    Add files via upload

---
+++ Torch.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Torch : MonoBehaviour
+{
+
+    private bool onOff = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void toggleFlash()
+    {
+        if (onOff == false)
+        {
+            CameraDevice.Instance.SetFlashTorchMode(true);
+            onOff = true;
+        }
+        else
+        {
+            CameraDevice.Instance.SetFlashTorchMode(false);
+            onOff = false;
+        }
+    }
+}
