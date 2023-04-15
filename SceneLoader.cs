aa3f3ec1221c218470cebb2a9f1f4cf401f97916
Alif Maulana Arifin <alifmaulana26@gmail.com>
2023-04-15 19:46:20 +0700

    update scene loader implementation to use sceneManager

--- SceneLoader.cs
+++ SceneLoader.cs
@@ -1,13 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-
     public void LoadScene(int level)
     {
-        Application.LoadLevel(level);
-    }
+        SceneManager.LoadScene(level);
+    }    
 }
